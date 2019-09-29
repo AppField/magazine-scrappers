736c7aa506ce0ff022d100efbd76854f39f5bc91
= <roman@muehlfeldner.net>
2019-09-29 10:41:56 +0200

    improved diepresse newsfeed scrapper

--- scrapper/diepresse/newsfeed.py
+++ scrapper/diepresse/newsfeed.py
@@ -11,15 +11,45 @@ class DiePresseNewsfeed(Newsfeed):
     directory = 'diepresse'
 
     def query_newsfeed(self):
+        aufmacher = self.soup.find('section', {'class': 'block--aufmacher'})
+        card_links = aufmacher.find_all('a', {'class': 'card__link'})
+        card_hrefs = [ link['href'] for link in card_links]
+
         items_list = self.soup.find('div', {'class': 'storylist--small'})
 
         items = items_list.find_all('li', {'class': 'storylist__item'})
         
         base_url = 'https://diepresse.com'
         self.links = [item.find('a')['href'] for item in items]        
+        self.links.extend(card_hrefs)
+
+
+    def write_links(self):
+        print(self.links)
+
+class DiePresseMeinungNewsfeed(Newsfeed):
+    directory = 'diepresse'
+
+    def query_newsfeed(self):
+        aufmacher = self.soup.find('section', {'class': 'block--aufmacher'})
+        card_links = aufmacher.find_all('a', {'class': 'card__link'})
+        card_hrefs = [ link['href'] for link in card_links]
+
+        items_list = self.soup.find('div', {'class': 'storylist--small'})
+
+        items = items_list.find_all('li', {'class': 'storylist__item'})
+        
+        base_url = 'https://diepresse.com'
+        self.links = [item.find('a')['href'] for item in items]        
+        self.links.extend(card_hrefs)
+
+
+    def write_links(self):
+        print(self.links)
+
 
 def main():
-    DiePresseNewsfeed('https://www.diepresse.com/')
+    DiePresseMeinungNewsfeed('https://www.diepresse.com/meinung')
 
 if __name__ == "__main__":
     main()
